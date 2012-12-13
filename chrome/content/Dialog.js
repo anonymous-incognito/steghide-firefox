3b72cf61b1f4734307cfcfaa5b0d6580d8004db9
Anonymous <hiddenchan@mail.i2p>
2012-12-14 00:00:03 +0700

    remove unused import

--- chrome/content/Dialog.js
+++ chrome/content/Dialog.js
@@ -20,7 +20,6 @@
 */
 
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
-Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
 Components.utils.import('resource://steghide-firefox/subprocess.jsm');
 
